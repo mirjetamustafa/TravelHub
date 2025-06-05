0807661c480df203539e879dda8cddb08ee368aa
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-06-05 11:15:48 +0200

    Flights finished

--- src/components/Flights.jsx
+++ src/components/Flights.jsx
@@ -1,20 +1,111 @@
 import React from 'react'
 import Table from './Table'
+import SkyFlights from './SkyFlights'
+
+const flights = [
+  {
+    name: 'SkyLine Airways',
+    departure: '8:30',
+    arrive: '14:45',
+    time: '7h 15m',
+    from: 'New York',
+    to: 'Paris',
+    fromAir: 'JFK',
+    toAir: 'CDG',
+    flightMode: 'Direct',
+    price: '485',
+    guest: 'per person',
+    rating: 4.2,
+  },
+  {
+    name: 'Global Wings',
+    departure: '11:20',
+    arrive: '18:30',
+    time: '8h 10m',
+    from: 'Tokyo',
+    to: 'London',
+    fromAir: 'HND',
+    toAir: 'LCY',
+    flightMode: '1 Stop',
+    price: '425',
+    guest: 'per person',
+    rating: 4,
+  },
+  {
+    name: 'Euro Express',
+    departure: '16:45',
+    arrive: '23:15',
+    time: '7h 30m',
+    from: 'Rome',
+    to: 'New York',
+    fromAir: 'FCO',
+    toAir: 'JFK',
+    flightMode: 'Direct',
+    price: '520',
+    guest: 'per person',
+    rating: 4.5,
+  },
+  {
+    name: 'SkyLine Airways',
+    departure: '8:30',
+    arrive: '14:45',
+    time: '7h 15m',
+    from: 'New York',
+    to: 'Paris',
+    fromAir: 'JFK',
+    toAir: 'CDG',
+    flightMode: 'Direct',
+    price: '485',
+    guest: 'per person',
+    rating: 4.2,
+  },
+  {
+    name: 'Global Wings',
+    departure: '11:20',
+    arrive: '18:30',
+    time: '8h 10m',
+    from: 'Tokyo',
+    to: 'London',
+    fromAir: 'HND',
+    toAir: 'LCY',
+    flightMode: '1 Stop',
+    price: '425',
+    guest: 'per person',
+    rating: 4,
+  },
+  {
+    name: 'Euro Express',
+    departure: '16:45',
+    arrive: '23:15',
+    time: '7h 30m',
+    from: 'Rome',
+    to: 'New York',
+    fromAir: 'FCO',
+    toAir: 'JFK',
+    flightMode: 'Direct',
+    price: '520',
+    guest: 'per person',
+    rating: 4.5,
+  },
+]
 
 const Flights = () => {
   return (
-    <main>
-      <section className="bg-blue-600 w-full mb-9 p-9 text-white">
+    <main className="">
+      <section className="bg-blue-600 w-full  p-9 text-white">
         <h1 className="font-bold md:text-4xl text-3xl mt-15">
           Find Your Perfect Flight
         </h1>
 
         <Table />
       </section>
-      <section className="mt-9">
+
+      <section className="pt-9 bg-gray-50">
         <div className="flex justify-between px-9 py-5">
           <div className="">
-            <h1 className="text-xl font-bold">3 flights found</h1>
+            <h1 className="text-xl font-bold">
+              {flights.length} flights found
+            </h1>
           </div>
           <form action="">
             <select className="outline outline-gray-400 rounded-md py-2 px-5 w-full text-gray-700">
@@ -24,6 +115,10 @@ const Flights = () => {
             </select>
           </form>
         </div>
+      </section>
+
+      <section className="p-9 bg-gray-50 ">
+        <SkyFlights flights={flights} />
       </section>
     </main>
   )
--- src/components/SkyFlights.jsx
+++ src/components/SkyFlights.jsx
@@ -1,36 +1,59 @@
-const flights = [
-  {
-    name: 'SkyLine Airways',
-    departure: '8:30',
-    arrive: '14:45',
-    time: '7h 15m',
-    from: 'New York',
-    to: 'Paris',
-    fromAir: 'JFK',
-    toAir: 'CDG',
-    flightMode: 'Direct',
-    price: '485',
-    guest: 'per person',
-    rating: 4.2,
-  },
-  {
-    name: 'Global Wings',
-    departure: '11:20',
-    arrive: '18:30',
-    time: '8h 10m',
-    from: 'New York',
-    to: 'Paris',
-    fromAir: 'JFK',
-    toAir: 'CDG',
-    flightMode: 'Direct',
-    price: '425',
-    guest: 'per person',
-    rating: 4,
-  },
-]
+import { LuPlane } from 'react-icons/lu'
+import { TiStarFullOutline } from 'react-icons/ti'
+import { CiClock2 } from 'react-icons/ci'
+import { FaArrowRight } from 'react-icons/fa'
 
-const SkyFlights = () => {
-  return <div></div>
+const SkyFlights = ({ flights }) => {
+  return (
+    <div className="">
+      {flights.map((flights, index) => (
+        <div
+          key={index}
+          className="bg-white shadow-sm rounded-md w-full h- px-9 py-5 my-5"
+        >
+          <div className="flex">
+            <LuPlane className="text-blue-600 mt-1" size={23} />
+            <h1 className="text-xl font-semibold mx-1">{flights.name}</h1>
+            <p className="flex text-gray-400 text-sm mt-2 mx-1">
+              <TiStarFullOutline className="text-yellow-400 mx-1" size={20} />
+              <span>{flights.rating}</span>
+            </p>
+          </div>
+          <div className="grid md:flex grid-cols-3 pt-5">
+            <div className="w-35">
+              <h1 className="text-2xl font-bold">{flights.departure}</h1>
+              <p className="text-gray-400 text-sm ml-2.5">{flights.fromAir}</p>
+              <p className="text-gray-400 text-sm">{flights.from}</p>
+              <div className="flex text-gray-600 mt-3 w-50">
+                <CiClock2 size={20} className="mr-1" />
+                <p className="text-sm">{flights.time}</p>
+                <p className="text-sm mx-1">{flights.flightMode}</p>
+              </div>
+            </div>
+            <div className="flex w-full mt-5">
+              <div className="border-b border-gray-300 w-120 h-5"></div>
+              <FaArrowRight className="text-gray-400 mt-3 mx-1 max-md:w-50" />
+              <div className="border-b border-gray-300 w-120 h-5"></div>
+            </div>
+            <div className="w-30 mx-6">
+              <h1 className="text-2xl font-bold">{flights.arrive}</h1>
+              <p className="text-gray-400 text-sm ml-5">{flights.toAir}</p>
+              <p className="text-gray-400 text-sm ml-5">{flights.to}</p>
+            </div>
+            <div className="w-70 ml-4 max-md:text-center max-md:mt-9">
+              <h1 className="text-4xl font-bold text-blue-600">
+                ${flights.price}
+              </h1>
+              <p className="text-sm text-gray-400 mx-3">{flights.guest}</p>
+              <button className="bg-blue-600 text-white rounded-md mt-5 px-3 py-2 font-bold cursor-pointer hover:bg-blue-700">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default SkyFlights
